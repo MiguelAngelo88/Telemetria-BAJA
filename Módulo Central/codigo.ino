3dece540de023267654c9a40a944d81920248e21
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2024-11-22 22:11:41 -0300

    Update codigo.ino

--- Módulo Central/codigo.ino
+++ Módulo Central/codigo.ino
@@ -1,1 +1,35 @@
+#include <CAN.h> //Inclui a biblioteca CAN
 
+void setup() {
+  Serial.begin(9600); //inicia a serial para debug
+  while (!Serial);
+
+  Serial.println("Receptor CAN");
+
+  // Inicia o barramento CAN a 500 kbps
+  if (!CAN.begin(500E3)) {
+    Serial.println("Falha ao iniciar o controlador CAN"); //caso não seja possível iniciar o controlador
+    while (1);
+  }
+}
+
+void loop() {
+  // Tenta verificar o tamanho do pacote recebido
+  int packetSize = CAN.parsePacket();
+
+  if (packetSize) {
+    Serial.println("Recebido pacote.");
+
+    if (!CAN.packetRtr()) { // Somente processa pacotes com dados
+      uint8_t rpmHigh = CAN.read(); // Primeiro byte (MSB)
+      uint8_t rpmLow = CAN.read();  // Segundo byte (LSB)
+
+      // Reconstruir o valor do RPM
+      uint16_t rpmInt = (rpmHigh << 8) | rpmLow;
+      float RPM = (float)rpmInt;
+
+      Serial.print("RPM recebido: ");
+      Serial.println(RPM);
+    }
+  }
+}
