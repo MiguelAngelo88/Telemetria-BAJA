4e7a4ce0dd907298da6c08bbfb4e9133eefa0234
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2024-11-24 09:23:15 -0300

    Update ModuloCentral.ino

--- Módulo Central/ModuloCentral.ino
+++ Módulo Central/ModuloCentral.ino
@@ -1,4 +1,42 @@
 #include <CAN.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <LoRa.h>
+#include <SPI.h>
+
+#define csPin 15
+
+/* Endereço I2C do display */
+#define OLED_ADDR 0x3c
+
+/* distancia, em pixels, de cada linha em relacao ao topo do display */
+#define OLED_LINE1 0
+#define OLED_LINE2 10
+#define OLED_LINE3 20
+#define OLED_LINE4 30
+#define OLED_LINE5 40
+#define OLED_LINE6 50
+
+/* Configuração da resolucao do display (este modulo possui display 128x64) */
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+/* Definicoes para comunicação com radio LoRa */
+#define SCK_LORA           5
+#define MISO_LORA          19
+#define MOSI_LORA          27
+#define RESET_PIN_LORA     14
+#define SS_PIN_LORA        18
+
+#define HIGH_GAIN_LORA     20  /* dBm */
+#define BAND               915E6  /* 915MHz de frequencia */
+
+/*
+   Variáveis e objetos globais
+*/
+
+/* objeto do display */
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 16);
 
 void setup() {
   Serial.begin(115200);
@@ -35,6 +73,8 @@ void loop() {
 
 // Inicializa a comunicação CAN
 void initializeCAN() {
+
+  CAN.setPins(csPin, 4);
   Serial.println("Inicializando o Transmissor CAN");
   if (!CAN.begin(500E3)) {
     Serial.println("Falha ao iniciar o controlador CAN");
