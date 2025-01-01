14489d0f0407e592da30836c4f1e0aa7f51813b0
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-01-01 09:59:44 -0300

    Add tempFreio

--- Módulo Central/ModuloCentral.ino
+++ Módulo Central/ModuloCentral.ino
@@ -15,22 +15,41 @@
 #define BAND               915E6  /* 915MHz de frequencia */
 
 /* typedefs */
-typedef struct __attribute__((__packed__))
-{
-  int rpmLoRa;
+typedef struct __attribute__((__packed__)) {
+  unsigned int rpmLoRa;
+  unsigned int tempFreio;
 } TDadosLora;
+
+/* Variáveis globais */
+unsigned int rpm = 0;
+unsigned int tempFreio = 0;
 
 void setup() {
   Serial.begin(115200);
 
   initializeCAN();  // Configura a comunicação CAN
 
-  /* Tenta, até obter sucesso, comunicacao com o chip LoRa */
+  /* Tenta, até obter sucesso, comunicação com o chip LoRa */
   while (init_comunicacao_lora() == false);
 }
 
 void loop() {
-  // Tenta analisar o pacote
+  if (recebe_dados_can()) {  // Se dados válidos foram recebidos
+    envia_informacoes_lora(rpm, tempFreio);
+  }
+}
+
+/* Função para inicializar a comunicação CAN */
+void initializeCAN() {
+  CAN.setPins(15, 4);
+  Serial.println("Inicializando o Transmissor CAN");
+  if (!CAN.begin(500E3)) {
+    Serial.println("Falha ao iniciar o controlador CAN");
+  }
+}
+
+/* Função para receber e processar dados CAN */
+bool recebe_dados_can() {
   int packetSize = CAN.parsePacket();
 
   if (packetSize) {  // Se um pacote for recebido
@@ -39,65 +58,57 @@ void loop() {
 
     // Verifica se o pacote contém dados
     if (!CAN.packetRtr() && packetSize == 2) {  // Deve conter exatamente 2 bytes
-      // Lê os dois bytes do RPM
-      int rpmHighByte = CAN.read();  // Primeiro byte (mais significativo)
-      int rpmLowByte = CAN.read();   // Segundo byte (menos significativo)
-
-      // Reconstrói o valor do RPM
-      unsigned int rpm = (rpmHighByte << 8) | rpmLowByte;
-
-      // Exibe o RPM no monitor serial
-      Serial.print("RPM recebido: ");
-      Serial.println(rpm);
-
-      envia_informacoes_lora(rpm);
+      if (CAN.packetId() == 0x15) {  // ID para RPM
+        int rpmHighByte = CAN.read();
+        int rpmLowByte = CAN.read();
+        rpm = (rpmHighByte << 8) | rpmLowByte;
+        Serial.print("RPM recebido: ");
+        Serial.println(rpm);
+        return true;
+      } else if (CAN.packetId() == 0x16) {  // ID para temperatura do freio
+        int tempHighByte = CAN.read();
+        int tempLowByte = CAN.read();
+        tempFreio = (tempHighByte << 8) | tempLowByte;
+        Serial.print("Temperatura do freio recebida: ");
+        Serial.println(tempFreio);
+        return true;
+      }
     } else {
       Serial.println("Pacote com tamanho ou formato inválido.");
     }
   }
+  return false;
 }
 
-// Inicializa a comunicação CAN
-void initializeCAN() {
-
-  CAN.setPins(15, 4);
-  Serial.println("Inicializando o Transmissor CAN");
-  if (!CAN.begin(500E3)) {
-    Serial.println("Falha ao iniciar o controlador CAN");
-  }
-}
-
-void envia_informacoes_lora(int rpm)
-{
- 
+/* Função para enviar informações via LoRa */
+void envia_informacoes_lora(unsigned int rpm, unsigned int tempFreio) {
   TDadosLora dados_lora;
 
   dados_lora.rpmLoRa = rpm;
+  dados_lora.tempFreio = tempFreio;
 
   LoRa.beginPacket();
-  LoRa.write((unsigned char *)&dados_lora, sizeof(TDadosLora));
+  LoRa.write((uint8_t*)&dados_lora, sizeof(TDadosLora));
   LoRa.endPacket();
 
-  Serial.print("LoRa enviou:");
-  Serial.println(rpm);
+  Serial.print("LoRa enviou - RPM: ");
+  Serial.print(rpm);
+  Serial.print(", TempFreio: ");
+  Serial.println(tempFreio);
 }
 
-
-bool init_comunicacao_lora(void)
-{
+/* Função para inicializar a comunicação LoRa */
+bool init_comunicacao_lora(void) {
   bool status_init = false;
   Serial.println("[LoRa Sender] Tentando iniciar comunicacao com o radio LoRa...");
   SPI.begin(SCK_LORA, MISO_LORA, MOSI_LORA, SS_PIN_LORA);
   LoRa.setPins(SS_PIN_LORA, RESET_PIN_LORA, LORA_DEFAULT_DIO0_PIN);
 
-  if (!LoRa.begin(BAND))
-  {
+  if (!LoRa.begin(BAND)) {
     Serial.println("[LoRa Sender] Comunicacao com o radio LoRa falhou. Nova tentativa em 1 segundo...");
     delay(1000);
     status_init = false;
-  }
-  else
-  {
+  } else {
     /* Configura o ganho do receptor LoRa para 20dBm, o maior ganho possível (visando maior alcance possível) */
     LoRa.setTxPower(HIGH_GAIN_LORA);
     Serial.println("[LoRa Sender] Comunicacao com o radio LoRa ok");
