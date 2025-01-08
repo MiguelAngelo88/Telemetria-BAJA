43e0089888b24e5095c6c175912b35f150e6aa20
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-01-08 12:11:43 -0300

    add nivel da bateria

--- Módulo Frontal/ModuloFrontal.ino
+++ Módulo Frontal/ModuloFrontal.ino
@@ -5,16 +5,19 @@
 hw_timer_t *wdTimer = NULL;
 
 // Definições e variáveis para o RPM DO MOTOR
-const int rpmPin = 13;  // Pino do sensor indutivo
+#define  rpmPin  13  // Pino do sensor indutivo
 volatile unsigned long pulseCount = 0;  // Contador de pulsos
 unsigned long lastTime = 0;
 const unsigned long interval = 1000;  // Intervalo de tempo para cálculo do RPM (1 segundo)
 
 // Definições e variáveis para a TEMPERATURA DO FREIO
-int thermoDO = 19;
-int thermoCS = 23;
-int thermoCLK = 5;
+#define thermoDO  19
+#define thermoCS  23
+#define thermoCLK  5
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);//declara o objeto MAX6675
+
+// Definições e variáveis para a SENSOR DE TENSÃO DA BATERIA
+#define batteryPin 12
 
 void setup() {
   Serial.begin(115200);  // Inicia a comunicação serial
@@ -27,8 +30,11 @@ void setup() {
 
 void loop() {
   timerWrite(wdTimer, 0);// reseta o timer do watchdog
+
   processRPM();  // Processa o cálculo e envio do RPM
   processTempFreio(); // Processa o cálculo e envio da Temp do freio
+  processBatteryLevel(); // Processa o cálculo e envio do nível da bateria
+
   delay(500);
 }
 
@@ -103,10 +109,34 @@ void processTempFreio() {
   Serial.println(" °C");
   
   // Envia a temperatura do freio via CAN
-  CAN.beginPacket(0x16);  // ID único para a temperatura do freio
+  CAN.beginPacket(0x16);  // ID em hexadecimal
   CAN.write((tempFreio >> 8) & 0xFF);  // Envia os 8 bits mais significativos
   CAN.write(tempFreio & 0xFF);        // Envia os 8 bits menos significativos
   CAN.endPacket();  // Encerra o pacote
   
   Serial.println("Temperatura do freio enviada via CAN.");
+}
+
+void processBatteryLevel() {
+  // Leitura da tensão da bateria
+  float batteryVoltage = analogRead(batteryPin) * 5 * 5.0 / 1023;
+
+  // Vetores com os limites das tensões e seus respectivos níveis de carga
+  const float voltageLevels[] = {12.60, 12.50, 12.42, 12.32, 12.20, 12.06, 11.90, 11.75, 11.58, 11.31, 10.50};
+  const int chargeLevels[] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0};
+
+  // Determinação do nível de carga
+  int batteryLevel = 0;
+  for (int i = 0; i < sizeof(voltageLevels) / sizeof(voltageLevels[0]); i++) {
+    if (batteryVoltage >= voltageLevels[i]) {
+      batteryLevel = chargeLevels[i];
+      break;  // Sai do loop ao encontrar o nível correspondente
+    }
+  }
+
+  // Envio do nível da bateria via CAN
+  CAN.beginPacket(0x17);  // ID em hexadecimal
+  CAN.write((batteryLevel >> 8) & 0xFF);  // Envia os 8 bits mais significativos
+  CAN.write(batteryLevel & 0xFF);        // Envia os 8 bits menos significativos
+  CAN.endPacket();  // Encerra o pacote
 }
