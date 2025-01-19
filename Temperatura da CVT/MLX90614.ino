afedc5034e83a4455c8eeddd41c12c60f767832e
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-01-19 11:19:51 -0300

    Update and rename Código to MLX90614.ino

---
+++ Temperatura da CVT/MLX90614.ino
@@ -0,0 +1,18 @@
+#include <wire.h><Wire.h><br>#include <adafruit_mlx90614.h></adafruit_mlx90614.h></wire.h><Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+ Serial.begin(9600);
+ Serial.println("Adafruit MLX90614 test");  
+ mlx.begin();   
+}
+
+void loop() {
+ Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC());
+ Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+ Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+ Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+ Serial.println();
+ delay(500);
+}
