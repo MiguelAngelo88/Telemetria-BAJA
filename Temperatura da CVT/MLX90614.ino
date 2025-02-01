f04ec11f9aafc6aacb97d143f6fd1bdfb09a87ef
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-02-01 12:08:12 -0300

    Update MLX90614.ino

--- Temperatura da CVT/MLX90614.ino
+++ Temperatura da CVT/MLX90614.ino
@@ -1,18 +1,28 @@
-#include <wire.h><Wire.h><br>#include <adafruit_mlx90614.h></adafruit_mlx90614.h></wire.h><Adafruit_MLX90614.h>
+#include <Adafruit_MLX90614.h>
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
- Serial.begin(9600);
- Serial.println("Adafruit MLX90614 test");  
- mlx.begin();   
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("Adafruit MLX90614 test");
+
+  if (!mlx.begin()) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
+
+  Serial.print("Emissivity = "); Serial.println(mlx.readEmissivity());
+  Serial.println("================================================");
 }
 
 void loop() {
- Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC());
- Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
- Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
- Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
- Serial.println();
- delay(500);
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC());
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF());
+  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+
+  Serial.println();
+  delay(500);
 }
