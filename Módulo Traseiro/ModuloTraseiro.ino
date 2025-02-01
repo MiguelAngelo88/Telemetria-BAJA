8427a9aaef02bb4acd93377046234b0ef53ecbcb
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-02-01 11:16:46 -0300

    Add nivel de combustível

--- Módulo Traseiro/ModuloTraseiro.ino
+++ Módulo Traseiro/ModuloTraseiro.ino
@@ -1,39 +1,48 @@
 #include <CAN.h>  // Inclui a biblioteca CAN
-#include <wire.h><Wire.h><br>#include <adafruit_mlx90614.h></adafruit_mlx90614.h></wire.h><Adafruit_MLX90614.h>
+#include <Adafruit_MLX90614.h>
 
-// Definições e variáveis para a Velocidade
-#define  wheelSensorPin  13  // Pino do sensor indutivo
+// Definições e variáveis para o RPM DO MOTOR
+#define  rpmPin  13  // Pino do sensor indutivo
 volatile unsigned long pulseCount = 0;  // Contador de pulsos
 unsigned long lastTime = 0;
 const unsigned long interval = 1000;  // Intervalo de tempo para cálculo do RPM (1 segundo)
-const float wheelRadius = 0.27; // Raio da roda em metros 
 
 // Definições e variáveis para a TEMPERATURA DA CVT
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
-// Definições e variáveis para a SENSOR DE TENSÃO DA BATERIA
-#define batteryPin 12
+// --- Definições e variáveis para o NÍVEL DE COMBUSTÍVEL ---
+const int sensor1Pin = 15; // Sensor inferior
+const int sensor2Pin = 16; // Sensor intermediário
+const int sensor3Pin = 17; // Sensor superior
+const unsigned long debounceDelay = 50; // Tempo de debounce em milissegundos
+struct SensorData {
+  int pin;
+  bool state;                  // Estado estável do sensor
+  bool lastReading;            // Última leitura bruta
+  unsigned long lastDebounceTime; // Último tempo em que ocorreu mudança
+};
+SensorData sensor1 = {sensor1Pin, LOW, LOW, 0};
+SensorData sensor2 = {sensor2Pin, LOW, LOW, 0};
+SensorData sensor3 = {sensor3Pin, LOW, LOW, 0};
 
 void setup() {
   Serial.begin(115200);  // Inicia a comunicação serial
   while (!Serial);
 
-  configureWatchdog();// Configura o watchdog
   initializeCAN();  // Configura a comunicação CAN
   initializeSensors();  // Configura o sensor indutivo
 }
 
 void loop() {
-  timerWrite(wdTimer, 0);// reseta o timer do watchdog
 
   processRPM();  // Processa o cálculo e envio do RPM
-  processTempFreio(); // Processa o cálculo e envio da Temp do freio
-  processBatteryLevel(); // Processa o cálculo e envio do nível da bateria
+  processTempCVT(); // Processa o cálculo e envio da Temp da CVT
+  processNivelCombustivel(); // Processa o cálculo e envio do nível do combustível
 
   delay(500);
 }
 
-void IRAM_ATTR handleWheelInterrupt() {
+void IRAM_ATTR handleInterrupt() {
   pulseCount++; // Incrementa o contador de pulsos na interrupção
 }
 
@@ -50,8 +59,21 @@ void initializeCAN() {
 void initializeSensors() {
   //RPM
   pinMode(rpmPin, INPUT_PULLUP);  
-  attachInterrupt(digitalPinToInterrupt(wheelSensorPin), handleWheelInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(rpmPin), handleInterrupt, FALLING);
   mlx.begin();
+
+  pinMode(sensor1.pin, INPUT_PULLUP);
+  pinMode(sensor2.pin, INPUT_PULLUP);
+  pinMode(sensor3.pin, INPUT_PULLUP);
+
+  sensor1.state = digitalRead(sensor1.pin);
+  sensor2.state = digitalRead(sensor2.pin);
+  sensor3.state = digitalRead(sensor3.pin);
+
+  sensor1.lastReading = sensor1.state;
+  sensor2.lastReading = sensor2.state;
+  sensor3.lastReading = sensor3.state;
+
 }
 
 // Função principal do loop para calcular e enviar o RPM
@@ -67,7 +89,7 @@ void processRPM() {
     Serial.println(rpm);
     
     // Envia o RPM via CAN
-    CAN.beginPacket(0x15);  // ID em hexadecimal
+    CAN.beginPacket(0x18);  // ID em hexadecimal
     CAN.write((rpm >> 8) & 0xFF);  // Envia os 8 bits mais significativos
     CAN.write(rpm & 0xFF);  // Envia os 8 bits menos significativos
     CAN.endPacket();  // Encerra o pacote
@@ -80,80 +102,76 @@ void processRPM() {
   }
 }
 
-void processVelocity() {
-  unsigned long currentTime = millis();
- 
-  // Calcula a velocidade a cada segundo
-  if (currentTime - lastTime >= interval) {
-    detachInterrupt(digitalPinToInterrupt(wheelSensorPin)); // Desabilita a interrupção temporariamente
-   
-    // Calcula o número de rotações
-    float rotations = pulseCount; // Supondo 1 pulso por rotação
-    // Calcula a velocidade angular (ω = 2π * rotações / tempo)
-    float omega = (2.0 * PI * rotations) / (interval / 1000.0); // rad/s
-    // Calcula a velocidade linear (v = ω * r)
-    float speed = omega * wheelRadius; // m/s
-    // Convertendo para km/h
-    float speedKmh = speed * 3.6;
-   
-    Serial.print("Velocidade: ");
-    Serial.print(speedKmh);
-    Serial.println(" km/h");
+void processTempCVT() {
+    float tempCVT = mlx.readObjectTempC(); // Lê a temperatura do sensor
 
-    // Envia o RPM via CAN
-    CAN.beginPacket(0x18);  // ID em hexadecimal
-    CAN.write((speedKmh >> 8) & 0xFF);  // Envia os 8 bits mais significativos
-    CAN.write(speedKmh & 0xFF);  // Envia os 8 bits menos significativos
+    Serial.print("Temperatura da CVT: ");
+    Serial.print(tempCVT);
+    Serial.println(" *C");
+
+    // Converte a temperatura para um formato adequado para envio via CAN
+    int16_t tempInt = static_cast<int16_t>(tempCVT * 100); // Multiplica por 100 para manter duas casas decimais
+
+    // Envia a temperatura via CAN
+    CAN.beginPacket(0x19);  // ID da mensagem CAN para temperatura da CVT
+    CAN.write((tempInt >> 8) & 0xFF);  // Envia os 8 bits mais significativos
+    CAN.write(tempInt & 0xFF);  // Envia os 8 bits menos significativos
     CAN.endPacket();  // Encerra o pacote
-    Serial.println("Velocidade enviado via CAN.");
-   
-    pulseCount = 0; // Reseta o contador de pulsos
-    lastTime = currentTime; // Atualiza o tempo
 
+    Serial.println("Temperatura da CVT enviada via CAN.");
+}
 
-    attachInterrupt(digitalPinToInterrupt(wheelSensorPin), handleWheelInterrupt, FALLING); // Reabilita a interrupção
+void updateSensor(SensorData &sensor) {
+  unsigned long currentTime = millis();
+  bool reading = digitalRead(sensor.pin);
+
+  if (reading != sensor.lastReading) {
+    sensor.lastDebounceTime = currentTime;
+  }
+
+  if ((currentTime - sensor.lastDebounceTime) > debounceDelay) {
+    sensor.state = reading;
+  }
+
+  sensor.lastReading = reading;
+}
+
+String getFuelLevel(bool s1, bool s2, bool s3) {
+  if (!s1 && !s2 && !s3) {
+    return "Tanque vazio";
+  } else if (s1 && !s2 && !s3) {
+    return "Nível muito baixo";
+  } else if (s1 && s2 && !s3) {
+    return "Nível médio";
+  } else if (s1 && s2 && s3) {
+    return "Tanque cheio ou nível alto";
+  } else {
+    return "Estado indefinido";
   }
 }
 
+// Função para processar o nível de combustível
+void processNivelCombustivel() {
+  updateSensor(sensor1);
+  updateSensor(sensor2);
+  updateSensor(sensor3);
 
-void processTempFreio() {
-  // Lê a temperatura em graus Celsius
-  int tempFreio = thermocouple.readCelsius();
+  String fuelLevel = getFuelLevel(sensor1.state, sensor2.state, sensor3.state);
   
-  // Imprime a temperatura no monitor serial
-  Serial.print("Temperatura do Freio: ");
-  Serial.print(tempFreio);
-  Serial.println(" °C");
-  
-  // Envia a temperatura do freio via CAN
-  CAN.beginPacket(0x16);  // ID em hexadecimal
-  CAN.write((tempFreio >> 8) & 0xFF);  // Envia os 8 bits mais significativos
-  CAN.write(tempFreio & 0xFF);        // Envia os 8 bits menos significativos
+  Serial.print("Nivel de Combustível: ");
+  Serial.println(fuelLevel);
+
+  // Envia o nível de combustível via CAN
+  uint8_t fuelLevelCode = 0;
+  if (fuelLevel == "Tanque vazio") fuelLevelCode = 0;
+  else if (fuelLevel == "Nível muito baixo") fuelLevelCode = 1;
+  else if (fuelLevel == "Nível médio") fuelLevelCode = 2;
+  else if (fuelLevel == "Tanque cheio ou nível alto") fuelLevelCode = 3;
+
+  // Envia o código do nível de combustível via CAN
+  CAN.beginPacket(0x20);  // ID da mensagem CAN para nível de combustível
+  CAN.write(fuelLevelCode);  // Envia o código do nível de combustível
   CAN.endPacket();  // Encerra o pacote
-  
-  Serial.println("Temperatura do freio enviada via CAN.");
-}
 
-void processBatteryLevel() {
-  // Leitura da tensão da bateria
-  float batteryVoltage = analogRead(batteryPin) * 5 * 5.0 / 1023;
-
-  // Vetores com os limites das tensões e seus respectivos níveis de carga
-  const float voltageLevels[] = {12.60, 12.50, 12.42, 12.32, 12.20, 12.06, 11.90, 11.75, 11.58, 11.31, 10.50};
-  const int chargeLevels[] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0};
-
-  // Determinação do nível de carga
-  int batteryLevel = 0;
-  for (int i = 0; i < sizeof(voltageLevels) / sizeof(voltageLevels[0]); i++) {
-    if (batteryVoltage >= voltageLevels[i]) {
-      batteryLevel = chargeLevels[i];
-      break;  // Sai do loop ao encontrar o nível correspondente
-    }
-  }
-
-  // Envio do nível da bateria via CAN
-  CAN.beginPacket(0x17);  // ID em hexadecimal
-  CAN.write((batteryLevel >> 8) & 0xFF);  // Envia os 8 bits mais significativos
-  CAN.write(batteryLevel & 0xFF);        // Envia os 8 bits menos significativos
-  CAN.endPacket();  // Encerra o pacote
+  Serial.println("Nivel de Combustível enviado via CAN.");
 }
