c7d0b8b53e9488c991e2a8735cd9bf6c476f2eae
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-02-01 10:03:29 -0300

    Correção de bugs e troca do RPM pela Velocidade

--- Módulo Frontal/ModuloFrontal.ino
+++ Módulo Frontal/ModuloFrontal.ino
@@ -1,14 +1,12 @@
 #include <CAN.h>  // Inclui a biblioteca CAN
 #include "max6675.h" //Sensor da temp do freio
 
-//Configuração do watchdog
-hw_timer_t *wdTimer = NULL;
-
-// Definições e variáveis para o RPM DO MOTOR
-#define  rpmPin  13  // Pino do sensor indutivo
+// Definições e variáveis para a Velocidade
+#define  wheelSensorPin  13  // Pino do sensor indutivo
 volatile unsigned long pulseCount = 0;  // Contador de pulsos
 unsigned long lastTime = 0;
 const unsigned long interval = 1000;  // Intervalo de tempo para cálculo do RPM (1 segundo)
+const float wheelRadius = 0.27; // Raio da roda em metros 
 
 // Definições e variáveis para a TEMPERATURA DO FREIO
 #define thermoDO  19
@@ -23,43 +21,27 @@ void setup() {
   Serial.begin(115200);  // Inicia a comunicação serial
   while (!Serial);
 
-  configureWatchdog();// Configura o watchdog
   initializeCAN();  // Configura a comunicação CAN
   initializeSensors();  // Configura o sensor indutivo
 }
 
 void loop() {
-  timerWrite(wdTimer, 0);// reseta o timer do watchdog
 
-  processRPM();  // Processa o cálculo e envio do RPM
+  processVelocity();  // Processa o cálculo e envio do RPM
   processTempFreio(); // Processa o cálculo e envio da Temp do freio
   processBatteryLevel(); // Processa o cálculo e envio do nível da bateria
 
   delay(500);
 }
 
-//Função que reinicia o ESP
-void IRAM_ATTR resetModule(){
-  ets_printf("(watchdog) reiniciar\n");
-  //esp_restart_noos();//reinicia o chip
-  esp_restart();
-}
-
 // Função de interrupção para contar pulsos do sensor indutivo
-void IRAM_ATTR handleInterrupt() {
+void IRAM_ATTR handleWheelInterrupt() {
   pulseCount++;
-}
-
-//Configura o watchdog
-void configureWatchdog(){
-  wdTimer = timerBegin(0,80, true); //timerID 0, 80 MHz
-  timerAttachInterrupt(wdTimer, &resetModule, true);
-  timerAlarmWrite(wdTimer, 3000000, true); //3000000 us = 3s
-  timerAlarmEnable(wdTimer);
 }
 
 // Inicializa a comunicação CAN
 void initializeCAN() {
+  CAN.setPins(15, 4);
   Serial.println("Inicializando o Transmissor CAN");
   if (!CAN.begin(500E3)) {
     Serial.println("Falha ao iniciar o controlador CAN");
@@ -69,33 +51,43 @@ void initializeCAN() {
 // Inicializa os sensores e configura a interrupções
 void initializeSensors() {
   //RPM
-  pinMode(rpmPin, INPUT_PULLUP);  
-  attachInterrupt(digitalPinToInterrupt(rpmPin), handleInterrupt, FALLING);
+  pinMode(wheelSensorPin, INPUT_PULLUP);  
+  attachInterrupt(digitalPinToInterrupt(wheelSensorPin), handleWheelInterrupt, FALLING);
 }
 
-// Função principal do loop para calcular e enviar o RPM
-void processRPM() {
+void processVelocity() {
   unsigned long currentTime = millis();
-  
+ 
+  // Calcula a velocidade a cada segundo
   if (currentTime - lastTime >= interval) {
-    detachInterrupt(digitalPinToInterrupt(rpmPin));  // Desabilita a interrupção temporariamente
-    
-    unsigned long rpm = (pulseCount * 60) / (interval / 1000); // Calcula o RPM
-    
-    Serial.print("RPM: ");
-    Serial.println(rpm);
-    
+    detachInterrupt(digitalPinToInterrupt(wheelSensorPin)); // Desabilita a interrupção temporariamente
+   
+    // Calcula o número de rotações
+    float rotations = pulseCount; // Supondo 1 pulso por rotação
+    // Calcula a velocidade angular (ω = 2π * rotações / tempo)
+    float omega = (2.0 * PI * rotations) / (interval / 1000.0); // rad/s
+    // Calcula a velocidade linear (v = ω * r)
+    float speed = omega * wheelRadius; // m/s
+    // Convertendo para km/h
+    float speedKmh = speed * 3.6;
+    int speedInt = (int)speedKmh; // Armazena apenas a parte inteira da velocidade
+   
+    Serial.print("Velocidade: ");
+    Serial.print(speedKmh);
+    Serial.println(" km/h");
+
     // Envia o RPM via CAN
     CAN.beginPacket(0x15);  // ID em hexadecimal
-    CAN.write((rpm >> 8) & 0xFF);  // Envia os 8 bits mais significativos
-    CAN.write(rpm & 0xFF);  // Envia os 8 bits menos significativos
+    CAN.write((speedInt >> 8) & 0xFF);  // Envia os 8 bits mais significativos
+    CAN.write(speedInt & 0xFF);  // Envia os 8 bits menos significativos
     CAN.endPacket();  // Encerra o pacote
-    Serial.println("RPM enviado via CAN.");
-    
-    pulseCount = 0;  // Reseta o contador de pulsos
-    lastTime = currentTime;  // Atualiza o tempo
-    
-    attachInterrupt(digitalPinToInterrupt(rpmPin), handleInterrupt, FALLING);  // Reabilita a interrupção
+    Serial.println("Velocidade enviado via CAN.");
+   
+    pulseCount = 0; // Reseta o contador de pulsos
+    lastTime = currentTime; // Atualiza o tempo
+
+
+    attachInterrupt(digitalPinToInterrupt(wheelSensorPin), handleWheelInterrupt, FALLING); // Reabilita a interrupção
   }
 }
 
