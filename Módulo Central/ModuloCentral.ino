8886c2e4d46169c39a8fe103a91b5b1c7cca8bca
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-04-19 11:05:54 -0300

    Otimização do envio dos dados para o Display

--- Módulo Central/ModuloCentral.ino
+++ Módulo Central/ModuloCentral.ino
@@ -9,17 +9,18 @@
 const byte rxPin = 16;  //rx2
 const byte txPin = 17;  //tx2
 SoftwareSerial mySerial(rxPin, txPin);
-
-#define velocidade 0x61
-#define rpmMotor 0x62
-#define tempFreio 0x63
-#define tempCVT 0x64
-
-unsigned char vel[8] = { 0x5a, 0xa5, 0x05, 0x82, velocidade, 0x00, 0x00, 0x00 };
-unsigned char rpm[8] = { 0x5a, 0xa5, 0x05, 0x82, rpm, 0x00, 0x00, 0x00 };
-unsigned char freio[8] = { 0x5a, 0xa5, 0x05, 0x82, freio, 0x00, 0x00, 0x00 };
-unsigned char cvt[8] = { 0x5a, 0xa5, 0x05, 0x82, cvt, 0x00, 0x00, 0x00 };
-
+#define DISP_ID_VELOCIDADE 0x61
+#define DISP_ID_RPM 0x62
+#define DISP_ID_FREIO 0x63
+#define DISP_ID_CVT 0x64
+#define DISP_ID_BATERIA 0x65
+#define DISP_ID_COMBUSTIVEL 0x66
+unsigned char vel[8] = { 0x5a, 0xa5, 0x05, 0x82, DISP_ID_VELOCIDADE, 0x00, 0x00, 0x00 };
+unsigned char rpm[8] = { 0x5a, 0xa5, 0x05, 0x82, DISP_ID_RPM, 0x00, 0x00, 0x00 };
+unsigned char freio[8] = { 0x5a, 0xa5, 0x05, 0x82, DISP_ID_FREIO, 0x00, 0x00, 0x00 };
+unsigned char cvt[8] = { 0x5a, 0xa5, 0x05, 0x82, DISP_ID_CVT, 0x00, 0x00, 0x00 };
+unsigned char bat[8] = { 0x5a, 0xa5, 0x05, 0x82, DISP_ID_BATERIA, 0x00, 0x00, 0x00 };
+unsigned char comb[8] = { 0x5a, 0xa5, 0x05, 0x82, DISP_ID_COMBUSTIVEL, 0x00, 0x00, 0x00 };
 
 /* Definicoes para comunicação com radio LoRa */
 #define SCK_LORA           5
@@ -27,7 +28,6 @@ unsigned char cvt[8] = { 0x5a, 0xa5, 0x05, 0x82, cvt, 0x00, 0x00, 0x00 };
 #define MOSI_LORA          27
 #define RESET_PIN_LORA     14
 #define SS_PIN_LORA        18
-
 #define HIGH_GAIN_LORA     20  /* dBm */
 #define BAND               915E6  /* 915MHz de frequencia */
 
@@ -62,7 +62,7 @@ void loop() {
     // Verifica se o pacote contém dados
     if (!CAN.packetRtr() && packetSize == 2) {  // Deve conter exatamente 2 bytes
 
-      if (CAN.packetId() == 0x15){// Lê os dois bytes do RPM
+      if (CAN.packetId() == 0x15){ //ID 0x15->Velocidade
       int velocidadeHighByte = CAN.read();  // Primeiro byte (mais significativo)
       int velocidadeLowByte = CAN.read();   // Segundo byte (menos significativo)
 
@@ -73,63 +73,93 @@ void loop() {
       Serial.print("Velocidade recebido: ");
       Serial.println(velocidade);
 
+      //Envia o valor da velocidade para o display
+      envia_para_display(vel, velocidade);
+
+      //Envia o valor da velocidade para o Box
       envia_informacoes_lora(velocidade,0,0,0,0,0);
-      } else if(CAN.packetId() == 0x16){
+
+      } else if(CAN.packetId() == 0x16){ //ID15->Temperatura do freio
       int freioHighByte = CAN.read();  // Primeiro byte (mais significativo)
       int freioLowByte = CAN.read();   // Segundo byte (menos significativo)
 
       // Reconstrói o valor da Temp do freio
       unsigned int tempFreio = (freioHighByte << 8) | freioLowByte;
 
-      // Exibe o RPM no monitor serial
+      // Exibe a temp do freio no monitor serial
       Serial.print("TempFreio recebido: ");
       Serial.println(tempFreio);
 
+      //Envia o valor da temp do freio para o display
+      envia_para_display(freio, tempFreio);
+
+      //Envia o valor da temp do freio para o Box
       envia_informacoes_lora(0,tempFreio,0,0,0,0);
-      } else if(CAN.packetId() == 0x17){
+
+      } else if(CAN.packetId() == 0x17){ //ID17->Nível da bateria
       int bateriaHighByte = CAN.read();  // Primeiro byte (mais significativo)
       int bateriaLowByte = CAN.read();   // Segundo byte (menos significativo)
 
       // Reconstrói o valor do Nivel da Bateria
       unsigned int NivelBateria = (bateriaHighByte << 8) | bateriaLowByte;
 
-      // Exibe o RPM no monitor serial
+      // Exibe o nivel da bateria no monitor serial
       Serial.print("NivelBateria recebido: ");
       Serial.println(NivelBateria);
 
+      //Envia o valor do nivel da bateria para o display
+      envia_para_display(bat, NivelBateria);
+
+      //Envia o valor da temp do freio para o Box
       envia_informacoes_lora(0,0,NivelBateria,0,0,0);
-      } else if(CAN.packetId() == 0x18){
+
+      } else if(CAN.packetId() == 0x18){ //ID18->RPM
       int rpmHighByte = CAN.read();  // Primeiro byte (mais significativo)
       int rpmLowByte = CAN.read();   // Segundo byte (menos significativo)
 
       // Reconstrói o valor do RPM
-      unsigned int rpm = (rpmHighByte << 8) | rpmLowByte;
+      unsigned int RPM = (rpmHighByte << 8) | rpmLowByte;
 
       // Exibe o RPM no monitor serial
       Serial.print("RPM recebido: ");
-      Serial.println(rpm);
+      Serial.println(RPM);
 
-      envia_informacoes_lora(0,0,0,rpm,0,0);
-      } else if(CAN.packetId() == 0x19){
+      //Envia o valor do RPM para o display
+      envia_para_display(rpm, RPM);
+
+      //Envia o valor do RPM para o Box
+      envia_informacoes_lora(0,0,0,RPM,0,0);
+
+      } else if(CAN.packetId() == 0x19){ //ID19->Temperatura da CVT
       int cvtHighByte = CAN.read();  // Primeiro byte (mais significativo)
       int cvtLowByte = CAN.read();   // Segundo byte (menos significativo)
 
       // Reconstrói o valor do Nivel da Bateria
-      unsigned int nivelCVT = (cvtHighByte << 8) | cvtLowByte;
+      unsigned int tempCVT = (cvtHighByte << 8) | cvtLowByte;
 
       // Exibe o RPM no monitor serial
-      Serial.print("NivelCVT recebido: ");
-      Serial.println(nivelCVT);
+      Serial.print("tempCVT recebido: ");
+      Serial.println(tempCVT);
 
-      envia_informacoes_lora(0,0,0,0,nivelCVT,0);
-    } else if(CAN.packetId() == 0x20){
+      //Envia o valor do RPM para o display
+      envia_para_display(cvt, tempCVT);
+
+      //Envia o valor da temp da cvt para o Box
+      envia_informacoes_lora(0,0,0,0,tempCVT,0);
+
+    } else if(CAN.packetId() == 0x20){ //ID20->Nível de combustível
       int nivelCombustivel = CAN.read(); 
 
-      // Exibe o RPM no monitor serial
+      // Exibe o nivel de combustivel no monitor serial
       Serial.print("NivelCombustivel recebido: ");
       Serial.println(nivelCombustivel);
 
-      envia_informacoes_lora(0,0,0,0,0,nivelCombustivel);     
+      //Envia o nivel de combustivel para o display
+      envia_para_display(comb, nivelCombustivel);
+
+      //Envia o valor do nivel de combustivel para o Box
+      envia_informacoes_lora(0,0,0,0,0,nivelCombustivel); 
+
     } else {
       Serial.println("Pacote com tamanho ou formato inválido.");
     }
@@ -137,7 +167,7 @@ void loop() {
  }
 }
 
-void envia_informacoes_lora(unsigned int velocidade, unsigned int temperaturaFreio, unsigned int nivelBateria, unsigned int rpm, unsigned int nivelCVT, unsigned int combustivel){
+void envia_informacoes_lora(unsigned int velocidade, unsigned int temperaturaFreio, unsigned int nivelBateria, unsigned int rpm, unsigned int tempCVT, unsigned int combustivel){
  
   TDadosLora dados_lora;
 
@@ -145,7 +175,7 @@ void envia_informacoes_lora(unsigned int velocidade, unsigned int temperaturaFre
   dados_lora.freioLoRa = temperaturaFreio;
   dados_lora.bateriaLoRa = nivelBateria;
   dados_lora.rpmLoRa = rpm;
-  dados_lora.cvtLoRa = nivelCVT;
+  dados_lora.cvtLoRa = tempCVT;
   dados_lora.combustivelLoRa = combustivel;
 
   LoRa.beginPacket();
@@ -162,8 +192,8 @@ void envia_informacoes_lora(unsigned int velocidade, unsigned int temperaturaFre
   Serial.print(nivelBateria);
   Serial.print(", RPM: ");
   Serial.print(rpm);
-  Serial.print(", NivelCVT: ");
-  Serial.print(nivelCVT);
+  Serial.print(", tempCVT: ");
+  Serial.print(tempCVT);
   Serial.print(", Combustivel: ");
   Serial.println(combustivel);
 }
@@ -200,10 +230,21 @@ bool init_comunicacao_lora(void){
   return status_init;
 }
 
-int send_Display() {
-  //Envia o valor da velocidade para o display
-  vel[6] = highByte(t);
-  vel[7] = lowByte(t);
-  mySerial.write(vel, 8);
- 
+void envia_para_display(unsigned char* pacote, unsigned int valor) {
+  pacote[6] = highByte(valor);
+  pacote[7] = lowByte(valor);
+  mySerial.write(pacote, 8);
 }
+
+/*OUTRA ABORDAGEM MAIS LEVE
+montar o pacote dentro da própria função, recebendo apenas o ID do display e o valor. Assim, você não precisa declarar um array diferente para cada variável. Exemplo:
+
+void envia_para_display(uint8_t id_display, unsigned int valor) {
+  unsigned char pacote[8] = { 0x5a, 0xa5, 0x05, 0x82, id_display, 0x00, highByte(valor), lowByte(valor) };
+  mySerial.write(pacote, 8);
+}
+
+E aí você chamaria assim:
+envia_para_display(DISP_ID_VELOCIDADE, velocidade);
+
+*/
