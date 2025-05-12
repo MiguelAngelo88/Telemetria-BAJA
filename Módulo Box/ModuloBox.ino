c9fa830af353df8aba57a5e5979780c2d42bb661
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-05-12 19:37:49 -0300

    Comunicação LoRa funcionando

--- Módulo Box/ModuloBox.ino
+++ Módulo Box/ModuloBox.ino
@@ -1,20 +1,27 @@
-#include <SPI.h>
-#include <LoRa.h>
+/*
+  Telemetria Veicular - Módulo do Box
+  Autor: Miguel Ângelo de Lacerda Silva
+  Data: 2025
+  Descrição: Recebimento dos dados via rádio LoRa e envio para dashboard em python
+*/
 
-/* Definições do rádio LoRa */
-constexpr int SCK_LORA   = 5;
-constexpr int MISO_LORA  = 19;
-constexpr int MOSI_LORA  = 27;
-constexpr int RESET_LORA = 14;
-constexpr int SS_LORA    = 18;
-constexpr float BAND     = 915E6;
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
 
-/* Definições do ID da equipe */
-constexpr uint8_t ID_EQUIPE = 58;
+#define RF_FREQUENCY                                915000000 // Hz
+#define TX_OUTPUT_POWER                             14        // dBm
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
 
-/* Estrutura de dados recebida */
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Payload size
+
 typedef struct __attribute__((__packed__)) {
-  uint8_t idEquipe;         
   uint16_t rpmLoRa;
   uint16_t velocidadeLoRa;
   uint16_t combustivelLoRa;
@@ -23,49 +30,57 @@ typedef struct __attribute__((__packed__)) {
   uint16_t cvtLoRa;
 } TDadosLora;
 
-TDadosLora dadosRecebidos;
+
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+int16_t rssi, rxSize;
+
+bool lora_idle = true;
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);
+    Serial.begin(115200);
+    Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
 
-  Serial.println("[LoRa Receiver] Inicializando...");
-
-  SPI.begin(SCK_LORA, MISO_LORA, MOSI_LORA, SS_LORA);
-  LoRa.setPins(SS_LORA, RESET_LORA, 26); // DIO0 padrão: 26 no ESP32
-
-  if (!LoRa.begin(BAND)) {
-    Serial.println("Falha ao iniciar LoRa!");
-    while (1);
-  }
-
-  Serial.println("[LoRa Receiver] OK!");
+    RadioEvents.RxDone = OnRxDone;
+    Radio.Init(&RadioEvents);
+    Radio.SetChannel(RF_FREQUENCY);
+    Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                      LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                      LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                      0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
 }
 
 void loop() {
-  int packetSize = LoRa.parsePacket();
-  
-  if (packetSize >= sizeof(TDadosLora)) {
-    Serial.println("[LoRa Receiver] Pacote recebido.");
-
-    // Ler dados
-    LoRa.readBytes((uint8_t*)&dadosRecebidos, sizeof(TDadosLora));
-
-    // Verificar ID
-    if (dadosRecebidos.idEquipe == ID_EQUIPE) {
-      Serial.println("[LoRa Receiver] Dados da minha equipe recebidos:");
-      Serial.printf("Velocidade: %u RPM: %u Combustível: %u Bateria: %u Freio: %u CVT: %u\n",
-                    dadosRecebidos.velocidadeLoRa,
-                    dadosRecebidos.rpmLoRa,
-                    dadosRecebidos.combustivelLoRa,
-                    dadosRecebidos.bateriaLoRa,
-                    dadosRecebidos.freioLoRa,
-                    dadosRecebidos.cvtLoRa);
-
-      // Aqui você pode enviar via Serial para a dashboard Python!
-      Serial.write((uint8_t*)&dadosRecebidos, sizeof(TDadosLora));
-    } else {
-      Serial.printf("[LoRa Receiver] Pacote de outra equipe (ID recebido: %u)\n", dadosRecebidos.idEquipe);
+    if (lora_idle) {
+        lora_idle = false;
+        Serial.println("Into RX mode");
+        Radio.Rx(0);
     }
-  }
+    Radio.IrqProcess();
+}
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssiParam, int8_t snr) {
+    rssi = rssiParam;
+    rxSize = size;
+
+    if (size == sizeof(TDadosLora)) {
+        TDadosLora dadosRecebidos;
+        memcpy(&dadosRecebidos, payload, sizeof(TDadosLora));
+
+        Serial.printf("\r\n[LoRa Recebido] vel: %u | freio: %u | bat: %u | rpm: %u | cvt: %u | comb: %u | RSSI: %d | Size: %d\r\n",
+                      dadosRecebidos.velocidadeLoRa,
+                      dadosRecebidos.freioLoRa,
+                      dadosRecebidos.bateriaLoRa,
+                      dadosRecebidos.rpmLoRa,
+                      dadosRecebidos.cvtLoRa,
+                      dadosRecebidos.combustivelLoRa,
+                      rssi, rxSize);
+    } else {
+        Serial.println("\r\n[LoRa Recebido] Tamanho inesperado de pacote!");
+    }
+
+    Radio.Sleep();
+    lora_idle = true;
 }
